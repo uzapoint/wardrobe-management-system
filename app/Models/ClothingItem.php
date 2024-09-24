fd160812cd98ce1cb0684ff2bb02b03c5df5b3f2
Donstesh <stephenshifoko@gmail.com>
2024-09-24 09:33:31 +0300

    clothing itme model and mifration

---
+++ app/Models/ClothingItem.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClothingItem extends Model
+{
+    protected $fillable = ['name', 'size', 'color', 'material', 'category_id', 'user_id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function outfits() {
+        return $this->belongsToMany(Outfit::class);
+    }
+}
