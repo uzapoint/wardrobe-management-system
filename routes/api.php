41766c23be673aaf1ffc6ff768f8d8f6425540e0
Collins Lagat <cklagat@gmail.com>
2024-09-24 12:58:09 +0300

    refactor: move to api routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use App\Http\Controllers\Auth\PasswordResetController;
+use App\Http\Controllers\Auth\RegisterUserController;
+use App\Http\Controllers\Auth\VerificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +23,26 @@ use Illuminate\Validation\ValidationException;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [RegisterUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/forgot-password', [PasswordResetController::class, 'sendResetLink'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::post('/reset-password', [PasswordResetController::class, 'changePassword'])
+    ->middleware('guest')
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', [VerificationController::class, "verify"])
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [VerificationController::class, 'sendNotification'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
 
 
 Route::post('/sanctum/token', function (Request $request) {
