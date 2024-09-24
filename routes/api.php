3f1d1224a0c6336e42587153412c089e5d1b052d
“willy <[email protected]>
2024-09-24 19:07:59 +0300

    impleamented the create, delete and update endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClotheItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::post('/clothe-items', [ClotheItemController::class, 'store']);
+Route::get('/clothe-items', [ClotheItemController::class, 'index']);
+Route::delete('/clothe-items/{id}', [ClotheItemController::class, 'destroy']);
+Route::put('/clothe-items/{id}', [ClotheItemController::class, 'update']);
+Route::get('/clothe-items/{id}', [ClotheItemController::class, 'show']);
